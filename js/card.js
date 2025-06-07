24e754188d17b9b2aa3d6cbdc1447fd418d49ab5
Vasek2024 <p.f1@yandex.ru>
2025-06-07 23:11:29 +0700

    Получение данных из запроса

--- js/card.js
+++ js/card.js
@@ -1,15 +1,43 @@
-import { Request } from './request.js'
+import { Request } from './request.js';
 export class Card{
-    constructor(){
-        this.request = new Request()
-        this.request.request()
+    constructor(search){
+        this.request = new Request(search) // вызываем класс Request (отправляя туда название книги)
+        this.data = this.request.request() // сохраняем то что вернул запрос в классе Request
     }
-    start(){
-        for(let i = 0; i<2; i++){
-            console.log(this.request);
-            // В ЭТОМ ЦИКЛЕ ХОЧУ ВЫВЕСТИ СЕКЩИИ (6 штук) С ТОВАРАМИ
-            // ВСТАВИВ ТО ЧТО ВЕРНУЛ class Request (из массива data.items)
-            // КАК ДО ЭТИХ ЗНАЧЕНИЙ ДОБРАТЬСЯ ?
-        }
+    start() {
+        // КАК В ПРОМИСЕ ДОБРАТЬСЯ ДО ЗНАЧЕНИЙ (типа заголовлк, картинка, описание),
+        // чтобы вставить их в разметку?
+        // В ЦИКЛЕ FOR ВМЕСТО i < 4 ВСТАВИТЬ ДЛИННУ ПОЛУЧЕННОГО МАССИВА ИЗ КЛАССА Request !!!
+    console.log(this.data)
+
+    for (let i = 0; i < 4; i++) {
+        this.mainArticle = document.querySelector('.main__article') // общиц блок под все карточки
+        this.mainArticle.innerHTML = `
+        <section class="main__article-section">
+                
+                <div class="main__section-carta">
+                    <img class="main__carta-img" src="./images/jpg/cover(1).jpg" alt="">
+                </div>
+                <div class="main__section-description">
+                    <h3 class="main__description-h3">Kevin Kwan</h3>
+                    <h1 class="main__description-h1">Crazy rich asians</h1>
+                    <div class="main__description-ratings">
+                        <img class="main__ratings-img" src="" alt="">
+                        <span class="main__ratings-num">252</span>
+                        <span class="main__ratings-review">review</span>
+                    </div>
+                    <div class="main__description-title">
+                        <p>the outrageously funny debut novel about three super-rich, pedigreed Chinese families and the gossip...</p>
+                    </div>
+                    <div class="main__description-price">
+                        <span class="main__price-currency">$</span>
+                        <span class="main__price-sum">4.99</span>
+                    </div>
+                    <button class="main__description-btn btn" type="button">buy now</button>
+                </div>
+                
+            </section>`
+    }
+
     }
 }
--- js/request.js
+++ js/request.js
@@ -1,17 +1,16 @@
 export class Request{
-    constructor(){
-this.apiKey = "AIzaSyCzhD-f6_4E5zbyaWjgn0RhaWv1qTVqfOY"
-this.search = "JavaScript"
-this.bookData = []
+    constructor(search){ // принимаем название книги
+        this.apiKey = "AIzaSyCzhD-f6_4E5zbyaWjgn0RhaWv1qTVqfOY" 
+        this.search = search
     }
-    request(){
-        fetch('https://www.googleapis.com/books/v1/volumes?q='+this.search+'&key='+this.apiKey+'&maxResults=6')
-        .then(response => response.json())
-        .then(data => {
-            this.bookData = data.items
-            // return this.bookData
-            // console.log(this.bookData)
-            // console.log(accessInfo.id);
-        })
+    async request(){
+        try{
+            const response = await fetch('https://www.googleapis.com/books/v1/volumes?q='+this.search+'&key='+this.apiKey+'&maxResults=6')
+            let data = await response.json();
+            return data
+        }
+        catch (err){
+            console.log('Запрос не прошёл!')
+        }
 }
 }
